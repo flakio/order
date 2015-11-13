4ba86186d42ad23c25cc22770636703a996a70ef
Trent Swanson <trentmswanson@hotmail.com>
2015-11-12 21:57:40 -0800

    updated order inster

--- src/api/order.js
+++ src/api/order.js
@@ -5,6 +5,7 @@
 	var mysql = require('mysql');
 	var process = require('process');
 	var wrapper = require('co-mysql');
+	var uuid = require('uuid');
 	
 	module.exports.list = function * (next) {
 		if ('GET' != this.method) return yield next;
@@ -12,7 +13,7 @@
 		//var connection = mysql.createConnection( process.env.CONNECTION_STRING );
 		var connection = wrapper(mysql.createConnection(process.env.MYSQL_ENDPOINT + '/flakio?debug=true'));
 
-		var results = yield connection.query ("SELECT * FROM `Order`");		
+		var results = yield connection.query ("SELECT * FROM `Order`");
 	
 		this.body = results;
 	}
@@ -24,12 +25,25 @@
 			limit: '1kb'
 		});
 		
+		var connection = wrapper(mysql.createConnection(process.env.MYSQL_ENDPOINT + '/flakio?debug=true'));
+		
+		yield connection.query("INSERT INTO `Order` (id, customerId, status, total, orderDate, shippingAddress) \
+		 VALUES (?, ?, ?, ?, NOW(), ?)",
+		 [uuid.v1(),
+		 data.customerId,
+		 data.status,
+		 data.total,
+		 data.shippingAddress]);
+		
 		//This is where we would call payment process or hold cc
 		
 		//Add a new order to MariaDB
 	}
 	
 	module.exports.getById = function * (id, next) {
+		
+		var connection = wrapper(mysql.createConnection(process.env.MYSQL_ENDPOINT + '/flakio?debug=true'));
+
 		//if (order.length === 0) {
 		//	this.throw(404, {error:'order with id = ' + id + ' was not found'});
 		//}
--- src/install/order.table.sql
+++ src/install/order.table.sql
@@ -2,11 +2,11 @@ CREATE TABLE IF NOT EXISTS `Order` (
 	`id` VARCHAR(45) NOT NULL ,
 	`customerId` VARCHAR(45) NOT NULL,
 	`status` INT NOT NULL ,
-	`total` DECIMAL(13,4) NULL ,
+	`total` DECIMAL(13,4) NOT NULL ,
 	`orderDate` DATE NOT NULL,
 	`shipDate` DATE NULL,
-	`processor` VARCHAR(45),
-	`txnId` VARCHAR(45),
-	`shippingAddress` VARCHAR(200),
+	`processor` VARCHAR(45) NULL,
+	`txnId` VARCHAR(45) NULL,
+	`shippingAddress` VARCHAR(200) NULL,
 	PRIMARY KEY (`id`)
 )
