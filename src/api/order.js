cdb2871ac4d82ce025f3cd9ee976feac5d0ca904
Trent Swanson <trentmswanson@hotmail.com>
2015-11-16 04:54:38 -0800

    code cleanup

--- src/api/order.js
+++ src/api/order.js
@@ -7,51 +7,65 @@
 	var wrapper = require('co-mysql');
 	var uuid = require('uuid');
 	
-	module.exports.list = function * (next) {
-		if ('GET' != this.method) return yield next;
-		
-		//var connection = mysql.createConnection( process.env.CONNECTION_STRING );
-		var connection = wrapper(mysql.createPool(settings.mysqlConnectionString() + '/flakio?debug=true'));
+	module.exports = {
 
-		var results = yield connection.query ("SELECT * FROM `Order`");
+		/*
+		* list()
+		* retrieve a list of orders
+		*/
+		list : function * (next) {
+			if ('GET' != this.method) return yield next;
+			
+			var connection = wrapper(mysql.createPool(settings.mysqlConnectionString() + '/flakio?debug=true'));
 	
-		this.body = results;
-	}
+			var results = yield connection.query ("SELECT * FROM `Order`");
+		
+			this.body = results;
+		},
+		
+		/*
+		* create()
+		* create a new order from data posted
+		*/
+		create : function * (next) {
+			
+			//Parse posted data
+			var data = yield parse(this, {
+				limit: '1kb'
+			});
+			
+			// Process payment ()
+			// In a real system this would involve multiple steps with some consistency checks in place
+			// We may even simply capture the payment and charge it when shipping
+			
+			// Add a new order to MariaDB
+			var connection = wrapper(mysql.createPool(settings.mysqlConnectionString() + '/flakio?debug=true'));
+			yield connection.query("INSERT INTO `Order` (id, customerId, email, status, total, orderDate, shippingAddress) \
+			VALUES (?, ?, ?, ?, ?, NOW(), ?)",
+			[uuid.v1(),
+			data.customerId,
+			data.email,
+			data.status,
+			data.total,
+			data.shippingAddress]);
+			
+			this.body = '';
+			
+			// TODO: Send email notification to notify service
+		},
+		
+		/*
+		* getById(id)
+		* retrieve order details using the order id passed
+		*/
+		getById : function * (id, next) {
+			
+			var connection = wrapper(mysql.createPool(settings.mysqlConnectionString() + '/flakio?debug=true'));
 	
-	module.exports.create = function * (next) {
-		
-		//Parse posted data
-		var data = yield parse(this, {
-			limit: '1kb'
-		});
-		
-		// Process payment ()
-		// In a real system this would involve multiple steps with some consistency checks in place
-		// We may even simply capture the payment and charge it when shipping
-		
-		// Add a new order to MariaDB
-		var connection = wrapper(mysql.createPool(settings.mysqlConnectionString() + '/flakio?debug=true'));
-		yield connection.query("INSERT INTO `Order` (id, customerId, email, status, total, orderDate, shippingAddress) \
-		 VALUES (?, ?, ?, ?, ?, NOW(), ?)",
-		 [uuid.v1(),
-		 data.customerId,
-		 data.email,
-		 data.status,
-		 data.total,
-		 data.shippingAddress]);
-		 
-		 this.body = '';
-		
-		// TODO: Send email notification to notify service
-	}
-	
-	module.exports.getById = function * (id, next) {
-		
-		var connection = wrapper(mysql.createPool(settings.mysqlConnectionString() + '/flakio?debug=true'));
-
-		//if (order.length === 0) {
-		//	this.throw(404, {error:'order with id = ' + id + ' was not found'});
-		//}
-		this.body = 'Done!';
+			//if (order.length === 0) {
+			//	this.throw(404, {error:'order with id = ' + id + ' was not found'});
+			//}
+			this.body = 'Done!';
+		}
 	}
 })();
