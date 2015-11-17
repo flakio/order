2dc93a4ed885c5354f390685dbd804f672b7c300
Trent Swanson <trentmswanson@hotmail.com>
2015-11-17 04:01:54 -0800

    error handling for install

--- src/api/install.js
+++ src/api/install.js
@@ -13,21 +13,27 @@
 		//var connection = mysql.createConnection( process.env.MYSQL_ENDPOINT);
 		//mysql://root:my-secret-pw@192.168.99.100
 		
-		// Create the Flak.io Order service database
-		var connection = wrapper(mysql.createPool(process.env.MYSQL_ENDPOINT + '/mysql?debug=true'));
-		var results = yield connection.query ("CREATE DATABASE IF NOT EXISTS flakio");
-		// Note: this probably does not need to be a pool
-		
-		connection = wrapper(mysql.createPool(process.env.MYSQL_ENDPOINT + '/flakio?debug=true'));
-
-		// Create the orders table
-		var statement = yield read('./install/order.table.sql', 'utf-8');
-		results = yield connection.query (statement);
-		
-		// Create the orders table
-		statement = yield read('./install/orderDetails.table.sql', 'utf-8');
-		results = yield connection.query (statement);
-		
-		this.body = "Awesome-sauce!  You should be ready to go";
+		try {
+			// Create the Flak.io Order service database
+			var connection = wrapper(mysql.createPool(process.env.MYSQL_ENDPOINT + '/mysql?debug=true'));
+			var results = yield connection.query ("CREATE DATABASE IF NOT EXISTS flakio");
+			// Note: this probably does not need to be a pool
+			
+			connection = wrapper(mysql.createPool(process.env.MYSQL_ENDPOINT + '/flakio?debug=true'));
+	
+			// Create the orders table
+			var statement = yield read('./install/order.table.sql', 'utf-8');
+			results = yield connection.query (statement);
+			
+			// Create the orders table
+			statement = yield read('./install/orderDetails.table.sql', 'utf-8');
+			results = yield connection.query (statement);
+			
+			this.body = "Awesome-sauce!  You should be ready to go";
+		}
+		catch(err) {
+			this.status = 500;
+			this.body = {error: err};
+		}
 	}
 })();
